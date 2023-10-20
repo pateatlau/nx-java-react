aee10b40b763e15d3c0ed8a6ca5a5ede2e7624ea
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-10-20 08:20:15 +0530

    Added console output for Hello Java

--- java/apps/hello-java/hello-java/src/main/java/com/example/demo/HelloJavaApplication.java
+++ java/apps/hello-java/hello-java/src/main/java/com/example/demo/HelloJavaApplication.java
@@ -8,6 +8,7 @@ public class HelloJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloJavaApplication.class, args);
+		System.out.println("Hello Java");
 	}
 
 }
