37ab8ca3ca9ef55b11d0fb6994a5717c59d057b3
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-10-27 12:51:31 +0530

    minor change in java-hello-2 and remote-mfe-2

--- java/apps/hello-java-2/hello-java-2/src/main/java/com/example/demo/HelloJava2Application.java
+++ java/apps/hello-java-2/hello-java-2/src/main/java/com/example/demo/HelloJava2Application.java
@@ -8,6 +8,7 @@ public class HelloJava2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloJava2Application.class, args);
+		System.out.println("Hello Java!!!");
 	}
 
 }
