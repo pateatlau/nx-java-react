46efd10ebebdb8b19080f0e685883e308372e22f
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-11-15 08:27:00 +0530

    Minor changes and unit test fixed in mfe2

--- java/apps/hello-java-2/hello-java-2/src/main/java/com/example/demo/HelloJava2Application.java
+++ java/apps/hello-java-2/hello-java-2/src/main/java/com/example/demo/HelloJava2Application.java
@@ -8,7 +8,7 @@ public class HelloJava2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloJava2Application.class, args);
-		System.out.println("Hello Java!!!");
+		System.out.println("Hello Java2!!");
 	}
 
 }
--- react/apps/remote-mfe-2/src/app/app.tsx
+++ react/apps/remote-mfe-2/src/app/app.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div>
-      <h2>Remote MFE 2!</h2>
+      <h2>Remote MFE 2!!</h2>
     </div>
   );
 }
